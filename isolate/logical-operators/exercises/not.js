46c53c1f98d8d63045048811a818c2989a1c6bb2
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-06-12 14:45:12 +0200

    wrong way

--- isolate/logical-operators/exercises/not.js
+++ isolate/logical-operators/exercises/not.js
@@ -6,38 +6,38 @@ function not(value) {
 
 }
 
-const not1 = !0;
-const isTrue1 = not1 === _;
+const not1 = not(0);
+const isTrue1 = not1 === true;
 console.assert(isTrue1, 'Test 1');
 
-const not2 = !false;
-const isTrue2 = not2 === _;
+const not2 = not(false);
+const isTrue2 = not2 === true;
 console.assert(isTrue2, 'Test 2');
 
-const not3 = !undefined;
-const isTrue3 = not3 === _;
+const not3 = not(undefined);
+const isTrue3 = not3 === true;
 console.assert(isTrue3, 'Test 3');
 
-const not4 = !null;
-const isTrue4 = not4 === _;
+const not4 = not(null);
+const isTrue4 = not4 === true;
 console.assert(isTrue4, 'Test 4');
 
-const not5 = !'';
-const isTrue5 = not5 === _;
+const not5 = not('');
+const isTrue5 = not5 === true;
 console.assert(isTrue5, 'Test 5');
 
-const not6 = !NaN;
-const isTrue6 = not6 === _;
+const not6 = not(NaN);
+const isTrue6 = not6 === true;
 console.assert(isTrue6, 'Test 6');
 
-const not7 = !'fdsa';
-const isTrue7 = not7 === _;
+const not7 = not('fdsa');
+const isTrue7 = not7 === false;
 console.assert(isTrue7, 'Test 7');
 
-const not8 = !'true';
-const isTrue8 = not8 === _;
+const not8 = not('true');
+const isTrue8 = not8 === false;
 console.assert(isTrue8, 'Test 8');
 
-const not9 = !1;
-const isTrue9 = not9 === _;
+const not9 = not(1);
+const isTrue9 = not9 === false;
 console.assert(isTrue9, 'Test 9');
