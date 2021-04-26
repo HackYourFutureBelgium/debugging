f5708d72eb87e254f6569b3a92fd5eec932f2015
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2021-04-26 12:01:50 +0200

    isolate updates

---
+++ isolate/17-break-continue/exercises/every-other-letter-1.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const word = 'pitsicola';
+
+let index = 0;
+let logThisOne = true;
+while (index < _._) {
+  index += 1;
+
+  logThisOne = !logThisOne;
+  if (!_) {
+    _;
+  }
+
+  const nextLetter = word[index];
+  console.log(index + ': ' + nextLetter); // i, s, c, l
+}
+
+console.assert(index === _._, 'there are this many letters in the word');
